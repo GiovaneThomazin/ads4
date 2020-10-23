ac41e071fd6e584c8e8eb4fb3f1f8a3dbc210ff4
GiovaneThomazin <69993080+GiovaneThomazin@users.noreply.github.com>
2020-10-22 21:14:16 -0300

    Update img.py

--- img.py
+++ img.py
@@ -1,25 +1,25 @@
-importar  os 
-from  flask  import  Flask , jsonify , request
-from  math  import  sqrt
+importar os 
+from flask import Flask, jsonify, request
+from  math import sqrt
 
 app  =  Flask(__name__)
 
 @app.route('/')
-def  nao_entre_em_panico():
-    proximo  =  1
-    anterior  =  0
-    limite  =  98
-    encontrado  =  0
-    resposta  =  "1, \ n "
-    while (encontrado  <  limite):
+def nao_entre_em_panico():
+    proximo = 1
+    anterior = 0
+    limite = 50 
+    found = 0
+    resposta = "0, "
+    while (found<limite) :
         tmp  =  proximo
         proximo = proximo + anterior
         anterior = tmp
-        encontrado = encontrado + 1
-        resposta +=  str ( proximo ) +  ", \ n "
+        found=found+1
+        resposta+= str(proximo) + ","
         
      resposta de retorno
 
-if  __name__  ==  "__main__" :
-    porta  =  int ( os . amb . get ( "PORT" , 5000 ))
-    app . executar ( host = '0.0.0.0' , porta = porta )
+if  __name__ == "__main__":
+    port = int (os.environ.get("PORT", 5000 ))
+    app.run(host='0.0.0.0', port=port)
